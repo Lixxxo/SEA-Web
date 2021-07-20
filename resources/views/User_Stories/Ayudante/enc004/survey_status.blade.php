995e045d24a289779d3d91ae59162a5915cc9d5f
Joeloop <73733157+Joeloop@users.noreply.github.com>
2021-07-20 00:19:16 -0400

    creacion grafico de torta ejemplo

--- resources/views/User_Stories/Ayudante/enc004/survey_status.blade.php
+++ resources/views/User_Stories/Ayudante/enc004/survey_status.blade.php
@@ -1,8 +1,31 @@
 @extends('layouts.base')
 @section('contenido')
-
-Soy Sísifo.
-
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ejemplo</title>
+  </head>
+  <body>
+    <!-- Chart's container -->
+    <div id="chart" style="height: 300px;"></div>
+    <!-- Charting library -->
+    <script src="https://unpkg.com/echarts/dist/echarts.min.js"></script>
+    <!-- Chartisan -->
+    <script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script>
+    <!-- Your application script -->
+    <script>
+      const chart = new Chartisan({
+        el: '#chart',
+        url: "@chart('survey_chart')",
+        hooks: new ChartisanHooks()
+             .colors(['#4299E1','#FE0045','#C07EF1','#67C560','#ECC94B'])
+                .datasets('pie')
+                .axis(false)
+      });
+    </script>
+  </body>
+</html>
 @endsection
 
 @section('script')
