275cdba1035603489b95195615a9a16caf88d907
Guetti <gustavo.szigethi@alumnos.ucn.cl>
2021-07-21 06:40:15 -0400

    Se muestra mensaje de que no hay encuestas

--- resources/views/User_Stories/student/answer_survey.blade.php
+++ resources/views/User_Stories/student/answer_survey.blade.php
@@ -9,34 +9,45 @@
     </fieldset>
 
     <div>
-        <table>
-            <thead>
-                <tr>
-                    <th id="header">nrc</th>
-                    <th id="header">código</th>
-                    <th id="header">rut profesor</th>
-                    <th id="header">nombre profesor</th>
-                    <th id="header">acción</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($student_course_list as $course)
-                    <tr>
-                        <td>{{ $course->nrc }}</td>
-                        <td>{{ $course->codigo_asignatura }}</td>
-                        <td>{{ $course->rut_profesor }}</td>
-                        <td>{{ $course->nombre_profesor }}</td>
-                        <td>
-                            <form action="">
-                                <input type="button" value="Responder encuesta">
-                                <input type="text" name="surveyId" hidden value={{ $course->Surveysid }}>
-                            </form>
-                        </td>
-                    </tr>
 
-                @endforeach
-            </tbody>
-        </table>
+        @if (count($student_course_list) == 0)
+            <p>No hay encuestas disponibles para este estudiante</p>
+        @else
+            <table>
+                <thead>
+                    <tr>
+                        <th id="header">nrc</th>
+                        <th id="header">código</th>
+                        <th id="header">rut profesor</th>
+                        <th id="header">nombre profesor</th>
+                        <th id="header">acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($student_course_list as $course)
+                        <tr>
+                            <td>{{ $course->nrc }}</td>
+                            <td>{{ $course->codigo_asignatura }}</td>
+                            <td>{{ $course->rut_profesor }}</td>
+                            <td>{{ $course->nombre_profesor }}</td>
+                            <td>
+                                <form action="">
+                                    @if ($course->Surveysid != null)
+                                        <input type="button" value="Responder encuesta">
+                                        <input type="text" name="surveyId" hidden value={{ $course->Surveysid }}>
+                                    @else
+                                        <p>Sin encuesta asociada</p>
+                                    @endif
+                                </form>
+                            </td>
+                        </tr>
+
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+
+
     </div>
 @endsection
 
