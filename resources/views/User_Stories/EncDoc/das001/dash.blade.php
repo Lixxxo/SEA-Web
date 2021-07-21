d0850e22c7385515f61bb636bbeda755d385bc3e
luis.rojas04@alumnos.ucn.cl <lixo20056208>
2021-07-21 17:36:00 -0400

    rows y col structure

--- resources/views/User_Stories/EncDoc/das001/dash.blade.php
+++ resources/views/User_Stories/EncDoc/das001/dash.blade.php
@@ -2,9 +2,15 @@
 @extends('layouts.base')
 
 @section('contenido')
-    
-    <div class="row"></div>
-    <div class="row"></div>
+    <div class="row">
+        <h4>Dashboard y la wea</h4>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">xD</div>
+        <div class="col-sm-4">xP</div>
+        <div class="col-sm-4">xF</div>
+    </div>
+
     <div class="row"></div>
 
 @endsection
