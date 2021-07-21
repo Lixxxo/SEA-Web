256f33e68dac1d3e8fac1d0817a51be0de8764e8
Guetti <gustavo.szigethi@alumnos.ucn.cl>
2021-07-21 19:18:17 -0400

    vista de dashboard

--- resources/views/User_Stories/EncDoc/das001/dash.blade.php
+++ resources/views/User_Stories/EncDoc/das001/dash.blade.php
@@ -1,19 +1,58 @@
-
 @extends('layouts.base')
 
+<title>Estadísticas globales</title>
 @section('contenido')
+
+    <div>
+        <a href="/dashboard">Menú principal</a>
+    </div>
+    <fieldset>
+        <legend>Visualizar indicadores</legend>
+    </fieldset>
     <div class="row">
-        <h4>Dashboard y la wea</h4>
+        <div class="col-sm-3">
+            <p>Encuestas respondidas</p>
+            <progress id="progress_all" value="32" max="100">   </progress>
+            <label for="progress_all">32%</label>
+            <p>Encuestas respondidas</p>
+            <progress id="progress_some" value="32" max="100">   </progress>
+            <label for="progress_some">32%</label>
+            <p>Encuestas respondidas</p>
+            <progress id="progress_none" value="32" max="100">   </progress>
+            <label for="progress_none">32%</label>
+
+
+        </div>
+        <div class="col-sm-3">
+            <p>Asignaturas sin encuestas activas</p>
+            <table>
+                <thead>
+                    <th class="header">nrc</th>
+                    <th class="header">código</th>
+                </thead>
+            </table>
+            <p>Asignaturas con encuestas activas sin respuestas</p>
+            <table>
+                <thead>
+                    <th class="header">nrc</th>
+                    <th class="header">código</th>
+                </thead>
+            </table>
+        </div>
+        <div class="col-sm-3">
+            <p>Indicadores por asignaturas</p>
+        </div>
+        <div class="col-sm-3">
+            <p>Indicadores por ayudantes</p>
+        </div>
     </div>
     <div class="row">
-        <div class="col-sm-4">xD</div>
-        <div class="col-sm-4">xP</div>
-        <div class="col-sm-4">xF</div>
+        <p>Consolidado por ayudante</p>
     </div>
 
     <div class="row"></div>
 
 @endsection
 @section('script')
-    
+
 @endsection
